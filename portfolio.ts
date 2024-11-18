256038ebbd9ae9e63561d5ed03c96e89c26e2de6
Sooraj Parameswaran <sooraj.parameswaran@agoda.com>
2024-11-18 21:58:46 +0700

    Update Agoda company description in portfolio.ts to remove redundant information

--- portfolio.ts
+++ portfolio.ts
@@ -246,7 +246,7 @@ export const experience: ExperienceType[] = [
     company: 'Agoda',
     companyLogo: '/img/icons/common/agoda-logo-blue.png',
     date: 'July 2024 – Present',
-    desc: 'Agoda.com is an online travel agency catering primarily to consumers in the Asia-Pacific region, with headquarters in Singapore and operations in Bangkok, Thailand. Agoda facilitates reservations for lodging, flights, ground transportation, and activities.',
+    desc: 'Agoda.com is an online travel agency catering primarily to consumers in the Asia-Pacific region, with headquarters in Singapore and operations in Bangkok, Thailand.',
     descBullets: [
       "Worked with the Flights platform team who are responsible for all flight searches and processing flight requests. Dealing with multiple requests per minute.",
       "Participated in multiple design reviews and code reviews inside the team."
